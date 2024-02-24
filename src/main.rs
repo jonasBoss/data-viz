299fdae6989ff5f097f7f3355c7b5faf356bc0db
Jonas Bosse <jonas.bosse@posteo.de>
2024-02-24 20:56:37 +0100

    spawn thread from gui button

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::{
-    default, io::{BufRead, BufReader}, sync::mpsc, thread, time::Duration
+    default,
+    io::{BufRead, BufReader},
+    sync::mpsc::{self, Receiver},
+    thread,
+    time::Duration,
 };
 
 use eframe::egui;
@@ -9,55 +13,64 @@ mod data_reader;
 
 use data_reader::{Frame, FrameReader};
 
-
 fn main() -> Result<(), eframe::Error> {
     env_logger::init();
-
-    let (frame_tx, frame_rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        let port = serialport::new("/dev/ttyUSB0", 115200)
-            .timeout(Duration::from_millis(100))
-            .open()
-            .unwrap();
-        let reader = BufReader::new(port);
-        let mut reader = FrameReader::new(reader);
-
-        loop {
-            let f = reader.next_frame().unwrap();
-            frame_tx.send(f).unwrap();
-        }
-    });
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1024.0, 740.0]),
         ..Default::default()
     };
 
-    eframe::run_native("Data Viz", options, Box::new(move|cc| Box::new(MyApp::new(cc, frame_rx))))
+    eframe::run_native(
+        "Data Viz",
+        options,
+        Box::new(move |cc| Box::new(MyApp::new(cc))),
+    )
 }
 
 struct MyApp {
     name: String,
     age: u32,
-    frame_rx: mpsc::Receiver<Frame>,
+    err: Option<String>,
+    sensor_id: u8,
+    frame_rx: Option<mpsc::Receiver<Frame>>,
     data: Vec<[f64; 2]>,
 }
 
 impl MyApp {
-    fn new(_cc: &eframe::CreationContext, mpsc: mpsc::Receiver<Frame> ) -> Self {
+    fn new(_cc: &eframe::CreationContext) -> Self {
         Self {
             name: "Jonas".to_owned(),
             age: 28,
-            frame_rx: mpsc,
+            err: None,
+            sensor_id: 1,
+            frame_rx: None,
             data: vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0]],
         }
+    }
+
+    fn spawn_reader(&mut self) {
+        let (frame_tx, frame_rx) = mpsc::channel();
+        thread::spawn(move || {
+            let port = serialport::new("/dev/ttyUSB0", 115200)
+                .timeout(Duration::from_millis(100))
+                .open()
+                .unwrap();
+            let reader = BufReader::new(port);
+            let mut reader = FrameReader::new(reader);
+
+            loop {
+                let f = reader.next_frame().unwrap();
+                frame_tx.send(f).unwrap();
+            }
+        });
+
+        self.frame_rx = Some(frame_rx);
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Data Viz");
             ui.horizontal(|ui| {
@@ -71,7 +84,11 @@ impl eframe::App for MyApp {
             }
             ui.label(format!("Hello {}, age {}", self.name, self.age));
 
-            while let Ok(f) =  self.frame_rx.try_recv() {
+            if ui.button("Run").clicked() && self.frame_rx.is_none() {
+                self.spawn_reader();
+            }
+
+            while let Some(Ok(f)) = self.frame_rx.as_ref().map(Receiver::try_recv) {
                 ui.label(format!("{f:?}"));
             }
         });
@@ -82,6 +99,10 @@ impl eframe::App for MyApp {
             plot.show(ui, |plt_ui| {
                 plt_ui.line(Line::new(PlotPoints::from(self.data.clone())).name("the data"));
             });
+        });
+
+        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui|{
+            self.err.as_ref().map(|err|ui.label(err));
         });
     }
 }
