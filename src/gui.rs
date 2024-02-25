52b4ad7125018d0b54ccedb0fb27725242f2fce0
Jonas Bosse <jonas.bosse@posteo.de>
2024-02-25 11:47:52 +0100

    reorganize stuff

--- src/gui.rs
+++ src/gui.rs
@@ -1,15 +1,16 @@
 use std::{
     collections::HashMap,
     io::{self, BufReader},
-    sync::mpsc::{self, TryRecvError},
+    sync::mpsc::{self, Receiver, Sender, TryRecvError},
     thread,
     time::Duration,
 };
 
-use eframe::egui::{self, Widget};
+use eframe::egui::{self, Ui, Widget};
 use egui_plot::{Legend, Line, Plot, PlotPoints};
 use itertools::Itertools;
 use log::error;
+use serialport::SerialPort;
 
 use crate::data_reader::{Frame, FrameReader, FrameReaderError};
 
@@ -25,6 +26,42 @@ pub struct MyApp {
     reader_comm: Option<(mpsc::Receiver<Frame>, mpsc::Sender<Commands>)>,
     /// {sensor_id -> {board_id -> data}}
     data: HashMap<u8, HashMap<u8, Vec<[f64; 2]>>>,
+}
+
+/// reader main function. Reads frames from the serial port and sends them into `frame_tx`
+fn reader(port: Box<dyn SerialPort>, frame_tx: Sender<Frame>, command_rx: Receiver<Commands>) {
+    let reader = BufReader::new(port);
+    let mut reader = FrameReader::new(reader);
+
+    loop {
+        match reader.next_frame() {
+            Ok(f) => match frame_tx.send(f) {
+                Ok(_) => (),
+                Err(e) => {
+                    error!("Main Thread has dropped the reciever");
+                    panic!("{e:?}")
+                }
+            },
+            Err(e @ FrameReaderError::RawDataError(_)) => error!("{e}"),
+            Err(FrameReaderError::IOError(e)) => match e.kind() {
+                io::ErrorKind::InvalidData => error!("{e}"),
+                io::ErrorKind::TimedOut => error!("{e}"),
+                _ => {
+                    error!("{e}");
+                    panic!("{e:?}")
+                }
+            },
+        }
+
+        match command_rx.try_recv() {
+            Ok(Commands::STOP) => return,
+            Err(TryRecvError::Empty) => (),
+            Err(e @ TryRecvError::Disconnected) => {
+                error!("Main thread dissapeared");
+                panic!("{e:?}")
+            }
+        }
+    }
 }
 
 impl MyApp {
@@ -47,42 +84,7 @@ impl MyApp {
 
         let (frame_tx, frame_rx) = mpsc::channel();
         let (command_tx, command_rx) = mpsc::channel();
-
-        thread::spawn(move || {
-            let reader = BufReader::new(port);
-            let mut reader = FrameReader::new(reader);
-
-            loop {
-                match reader.next_frame() {
-                    Ok(f) => match frame_tx.send(f) {
-                        Ok(_) => (),
-                        Err(e) => {
-                            error!("Main Thread has dropped the reciever");
-                            panic!("{e:?}")
-                        }
-                    },
-                    Err(e @ FrameReaderError::RawDataError(_)) => error!("{e}"),
-                    Err(FrameReaderError::IOError(e)) => match e.kind() {
-                        io::ErrorKind::InvalidData => error!("{e}"),
-                        io::ErrorKind::TimedOut => error!("{e}"),
-                        _ => {
-                            error!("{e}");
-                            panic!("{e:?}")
-                        }
-                    },
-                }
-
-                match command_rx.try_recv() {
-                    Ok(Commands::STOP) => return,
-                    Err(TryRecvError::Empty) => (),
-                    Err(e @ TryRecvError::Disconnected) => {
-                        error!("Main thread dissapeared");
-                        panic!("{e:?}")
-                    }
-                }
-            }
-        });
-
+        thread::spawn(|| reader(port, frame_tx, command_rx));
         self.reader_comm = Some((frame_rx, command_tx));
         Ok(())
     }
@@ -116,6 +118,74 @@ impl MyApp {
         }
         false
     }
+
+    fn show_sidebar(&mut self, ui: &mut Ui) {
+        ui.heading("Data Viz");
+        egui::Grid::new("control_area").show(ui, |ui| {
+            let size = [100.0, 0.0].into();
+            ui.label("Port:");
+            egui::TextEdit::singleline(&mut self.path)
+                .min_size(size)
+                .show(ui);
+            ui.end_row();
+
+            ui.label("Baudrate:");
+            ui.label(format!("{}", self.baud));
+            ui.end_row();
+
+            ui.label("");
+
+            if let Some((_, ref sender)) = self.reader_comm {
+                if egui::Button::new("Stop reading")
+                    .min_size(size)
+                    .ui(ui)
+                    .clicked()
+                {
+                    let _ = sender.send(Commands::STOP);
+                }
+            } else if egui::Button::new("Start reading")
+                .min_size(size)
+                .ui(ui)
+                .clicked()
+            {
+                if let Err(e) = self.spawn_reader() {
+                    self.err = Some(format!("{e}"));
+                }
+            }
+
+            ui.end_row();
+            ui.label("");
+            if egui::Button::new("Clear Data")
+                .min_size(size)
+                .ui(ui)
+                .clicked()
+            {
+                self.data.clear()
+            }
+        });
+        for id in self.data.keys().sorted() {
+            let mut selected = id == &self.sensor_id;
+            ui.toggle_value(&mut selected, format!("Show Sensor {id}"));
+            if selected {
+                self.sensor_id = *id;
+            }
+        }
+    }
+
+    fn show_plot(&mut self, ui: &mut Ui) {
+        ui.heading(format!("Sensor: {}", self.sensor_id));
+        if let Some(data) = self.data.get(&self.sensor_id) {
+            let plot = Plot::new("sensor_plt").legend(Legend::default());
+            plot.show(ui, |plt_ui| {
+                for (board_id, data) in data.iter() {
+                    plt_ui.line(
+                        Line::new(PlotPoints::from(data.clone()))
+                            .name(format!("Board id: {board_id}")),
+                    );
+                }
+            });
+        }
+    }
 }
 
 impl eframe::App for MyApp {
@@ -124,74 +194,8 @@ impl eframe::App for MyApp {
             ctx.request_repaint_after(Duration::from_millis(100));
         }
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Data Viz");
-            egui::Grid::new("control_area").show(ui, |ui| {
-                let size = [100.0, 0.0].into();
-                ui.label("Port:");
-                egui::TextEdit::singleline(&mut self.path)
-                    .min_size(size)
-                    .show(ui);
-                ui.end_row();
-
-                ui.label("Baudrate:");
-                ui.label(format!("{}", self.baud));
-                ui.end_row();
-
-                ui.label("");
-
-                if let Some((_, ref sender)) = self.reader_comm {
-                    if egui::Button::new("Stop reading")
-                        .min_size(size)
-                        .ui(ui)
-                        .clicked()
-                    {
-                        let _ = sender.send(Commands::STOP);
-                    }
-                } else if egui::Button::new("Start reading")
-                    .min_size(size)
-                    .ui(ui)
-                    .clicked()
-                {
-                    if let Err(e) = self.spawn_reader() {
-                        self.err = Some(format!("{e}"));
-                    }
-                }
-
-                ui.end_row();
-                ui.label("");
-                if egui::Button::new("Clear Data")
-                    .min_size(size)
-                    .ui(ui)
-                    .clicked()
-                {
-                    self.data.clear()
-                }
-            });
-            for id in self.data.keys().sorted() {
-                let mut selected = id == &self.sensor_id;
-                ui.toggle_value(&mut selected, format!("Show Sensor {id}"));
-                if selected {
-                    self.sensor_id = *id;
-                }
-            }
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(format!("Sensor: {}", self.sensor_id));
-            if let Some(data) = self.data.get(&self.sensor_id) {
-                let plot = Plot::new("sensor_plt").legend(Legend::default());
-                plot.show(ui, |plt_ui| {
-                    for (board_id, data) in data.iter() {
-                        plt_ui.line(
-                            Line::new(PlotPoints::from(data.clone()))
-                                .name(format!("Board id: {board_id}")),
-                        );
-                    }
-                });
-            }
-        });
-
+        egui::SidePanel::left("side_panel").show(ctx, |ui| self.show_sidebar(ui));
+        egui::CentralPanel::default().show(ctx, |ui| self.show_plot(ui));
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
             self.err.as_ref().map(|err| ui.label(err));
         });
