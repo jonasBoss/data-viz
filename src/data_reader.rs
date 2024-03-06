9d40e9aef3d4f92708fa9a4abf7a3ef446b6b871
Jonas Bosse <jonas.bosse@posteo.de>
2024-03-06 18:11:23 +0100

    make reader struct that hanldes frame reader thread and state

--- src/data_reader.rs
+++ src/data_reader.rs
@@ -1,32 +1,204 @@
-use std::io::{self, BufRead, BufReader};
+use std::{
+    collections::HashMap,
+    io::{self, BufRead, BufReader},
+    sync::mpsc::{self, TryRecvError},
+    thread,
+    time::Duration,
+};
 
 use lazy_static::lazy_static;
+use log::error;
 use regex::Regex;
 use serialport::SerialPort;
 
-#[derive(Debug)]
-pub struct Frame {
-    pub board_id: u8,
-    pub sensor_id: u8,
-    pub value: u16,
-    pub timestamp: u32,
+enum Commands {
+    Stop,
 }
 
 #[derive(Debug)]
-pub struct FrameReader {
+struct Frame {
+    board_id: u8,
+    sensor_id: u8,
+    value: u16,
+    timestamp: u32,
+}
+
+#[derive(Debug, Default)]
+pub struct Reader {
+    comm: Option<ReaderComm>,
+    status: ReaderStatus,
+    /// {(board_id, sensor_id) ->  data}
+    pub data: HashMap<(u8, u8), Vec<[f64; 2]>>,
+}
+
+#[derive(Debug, Default)]
+enum ReaderStatus {
+    Err(String),
+    Running,
+    #[default]
+    Stopped,
+}
+
+#[derive(Debug)]
+struct ReaderComm {
+    command_tx: mpsc::Sender<Commands>,
+    frame_rx: mpsc::Receiver<Frame>,
+    status_rx: mpsc::Receiver<ReaderStatus>,
+}
+
+#[derive(Debug)]
+struct FrameReader {
     port: BufReader<Box<dyn SerialPort>>,
     buf: String,
 }
 
+impl Reader {
+    /// update internal state, returns duration after which this should be called again
+    pub fn process(&mut self) -> Option<Duration> {
+        let Some(ref mut r) = self.comm else {
+            return None;
+        };
+
+        let mut ret = None;
+
+        let e = loop {
+            match r.frame_rx.try_recv() {
+                Ok(f) => self
+                    .data
+                    .entry((f.board_id, f.sensor_id))
+                    .or_default()
+                    .push([f.timestamp as f64, f.value as f64]),
+                Err(e) => break e,
+            }
+        };
+
+        match e {
+            TryRecvError::Empty => ret = Some(Duration::from_millis(100)),
+            TryRecvError::Disconnected => {
+                self.status = ReaderStatus::Err("Reader Disconnected unexpectedly".into())
+            }
+        };
+
+        match r.status_rx.try_recv() {
+            Ok(ReaderStatus::Running) => {
+                self.status = ReaderStatus::Running;
+                ret
+            }
+            Ok(s) => {
+                self.status = s;
+                self.comm = None;
+                None
+            }
+            Err(TryRecvError::Disconnected) => {
+                self.status = ReaderStatus::Err("Reader Disconnected unexpectedly".into());
+                self.comm = None;
+                None
+            }
+            Err(TryRecvError::Empty) => ret,
+        }
+    }
+
+    pub fn start_reading(&mut self, path: &str, baud: u32) {
+        if self.comm.is_none() {
+            match Self::spawn_reader(path, baud) {
+                Ok(r) => self.comm = Some(r),
+                Err(e) => self.status = ReaderStatus::Err(format!("{e}")),
+            }
+        }
+    }
+
+    pub fn stop_reading(&mut self) {
+        let Some(ref mut r) = self.comm else {
+            return;
+        };
+        let _ = r.command_tx.send(Commands::Stop);
+    }
+
+    pub fn running(&self) -> bool {
+        matches!(self.status, ReaderStatus::Running)
+    }
+
+    pub fn reader_status(&self) -> String {
+        match self.status {
+            ReaderStatus::Err(ref e) => e.to_owned(),
+            ReaderStatus::Running => "Running".to_owned(),
+            ReaderStatus::Stopped => "Stopped".to_owned(),
+        }
+    }
+
+    fn spawn_reader(path: &str, baud: u32) -> Result<ReaderComm, io::Error> {
+        let port = serialport::new(path, baud)
+            .timeout(Duration::from_millis(100))
+            .open()?;
+
+        let (frame_tx, frame_rx) = mpsc::channel();
+        let (command_tx, command_rx) = mpsc::channel();
+        let (status_tx, status_rx) = mpsc::channel();
+
+        thread::spawn(|| Self::reader_main(port, frame_tx, status_tx, command_rx));
+        Ok(ReaderComm {
+            command_tx,
+            frame_rx,
+            status_rx,
+        })
+    }
+
+    /// reader main function. Reads frames from the serial port and sends them into `frame_tx`
+    fn reader_main(
+        port: Box<dyn SerialPort>,
+        frame_tx: mpsc::Sender<Frame>,
+        status_tx: mpsc::Sender<ReaderStatus>,
+        command_rx: mpsc::Receiver<Commands>,
+    ) {
+        let reader = BufReader::new(port);
+        let mut reader = FrameReader::new(reader);
+        let mut err_retry = 0u8;
+        status_tx
+            .send(ReaderStatus::Running)
+            .expect("Main Thread dropped status reciver");
+        loop {
+            match command_rx.try_recv() {
+                Ok(Commands::Stop) => {
+                    status_tx
+                        .send(ReaderStatus::Stopped)
+                        .expect("Main Thread dropped status reciver");
+                    return;
+                }
+                Err(mpsc::TryRecvError::Empty) => (),
+                Err(mpsc::TryRecvError::Disconnected) => {
+                    panic!("Main Thread dropped command sender")
+                }
+            }
+
+            match reader.next_frame() {
+                Ok(f) => {
+                    err_retry = 0;
+                    frame_tx.send(f).expect("Main Thread dropped frame reciver");
+                }
+                Err(e) => {
+                    err_retry += 1;
+                    error!("{e:?}");
+                    if err_retry > 3 {
+                        status_tx
+                            .send(ReaderStatus::Err(e.to_string()))
+                            .expect("Main Thread dropped status reciver");
+                        panic!("Too many read errors")
+                    }
+                }
+            };
+        }
+    }
+}
+
 impl FrameReader {
-    pub fn new(port: BufReader<Box<dyn SerialPort>>) -> Self {
+    fn new(port: BufReader<Box<dyn SerialPort>>) -> Self {
         Self {
             port,
             buf: String::with_capacity(64),
         }
     }
 
-    pub fn next_frame(&mut self) -> io::Result<Frame> {
+    fn next_frame(&mut self) -> io::Result<Frame> {
         self.port.read_line(&mut self.buf)?;
         let res = self.buf.as_str().try_into();
         self.buf.clear();
--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,7 @@
 use std::{
-    collections::{HashMap, HashSet}, io::{self, BufReader}, path::{Path, PathBuf}, sync::mpsc::{self, Receiver, Sender, TryRecvError}, thread, time::Duration
+    collections::HashSet,
+    io::{self},
+    path::Path,
 };
 
 use dirs::home_dir;
@@ -7,160 +9,37 @@ use eframe::egui::{self, Ui, Widget};
 use egui_file::FileDialog;
 use egui_plot::{Legend, Line, Plot, PlotPoints};
 use itertools::Itertools;
-use log::{error, info};
-use serialport::SerialPort;
+use log::info;
 
-use crate::data_reader::{Frame, FrameReader};
-
-enum Commands {
-    STOP,
-}
+use crate::data_reader::Reader;
 
 pub struct MyApp {
     path: String,
     baud: u32,
-    err: Option<String>,
 
     sensors: HashSet<u8>,
     boards: HashSet<u8>,
 
-    reader_comm: Option<(Receiver<io::Result<Frame>>, Sender<Commands>)>,
-    /// {(board_id, sensor_id) ->  data}
-    data: HashMap<(u8, u8), Vec<[f64; 2]>>,
+    reader: Reader,
     file_dialog: FileDialog,
-}
-
-/// reader main function. Reads frames from the serial port and sends them into `frame_tx`
-fn reader(
-    port: Box<dyn SerialPort>,
-    frame_tx: Sender<io::Result<Frame>>,
-    command_rx: Receiver<Commands>,
-) {
-    let reader = BufReader::new(port);
-    let mut reader = FrameReader::new(reader);
-
-    let mut error = false;
-    loop {
-        match command_rx.try_recv() {
-            Ok(Commands::STOP) => break,
-            Err(TryRecvError::Empty) => (),
-            Err(e @ TryRecvError::Disconnected) => {
-                error!("Main thread dissapeared");
-                panic!("{e:?}")
-            }
-        }
-
-        let msg = reader.next_frame();
-        match msg {
-            Ok(_) => (),
-            Err(ref e) => match e.kind() {
-                io::ErrorKind::InvalidData => (),
-                io::ErrorKind::TimedOut => (),
-                _ => {
-                    error = true;
-                }
-            },
-        }
-        match frame_tx.send(msg) {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Main Thread has dropped the reciever");
-                panic!("{e:?}")
-            }
-        }
-        if error {
-            break;
-        }
-    }
 }
 
 impl MyApp {
     pub fn new(_cc: &eframe::CreationContext) -> Self {
         let file_dialog = FileDialog::save_file(home_dir())
             .default_filename("sensor_data.csv")
-            .filename_filter(Box::new(|s:&str| s.ends_with(".csv")))
-            .show_files_filter(Box::new(|s:&Path| s.ends_with(".csv")));
+            .filename_filter(Box::new(|s: &str| s.ends_with(".csv")))
+            .show_files_filter(Box::new(|s: &Path| {
+                s.extension().is_some_and(|ext| ext == "csv")
+            }));
 
         Self {
             path: "/dev/ttyUSB0".to_owned(),
             baud: 921_600,
-            err: None,
-            reader_comm: None,
-            data: Default::default(),
+            reader: Default::default(),
             sensors: Default::default(),
             boards: Default::default(),
             file_dialog,
-        }
-    }
-
-    fn spawn_reader(&mut self) -> Result<(), io::Error> {
-        self.data.clear();
-        let port = serialport::new(self.path.clone(), self.baud)
-            .timeout(Duration::from_millis(100))
-            .open()?;
-
-        let (frame_tx, frame_rx) = mpsc::channel();
-        let (command_tx, command_rx) = mpsc::channel();
-        thread::spawn(|| reader(port, frame_tx, command_rx));
-        self.reader_comm = Some((frame_rx, command_tx));
-        Ok(())
-    }
-
-    /// reads data from the mpsc
-    ///
-    /// **returns** the duration after which to redraw
-    fn recive_data(&mut self) -> Option<Duration> {
-        let ch = self.reader_comm.as_ref()?;
-
-        loop {
-            match ch.0.try_recv() {
-                Ok(Ok(f)) => {
-                    self.data
-                        .entry((f.board_id, f.sensor_id))
-                        .or_default()
-                        .push([f.timestamp as f64, f.value as f64]);
-                }
-
-                Ok(Err(e)) => return self.handle_reader_error(e),
-
-                Err(TryRecvError::Disconnected) => {
-                    self.reader_comm = None;
-                    self.err = Some("Reader Disconected".into());
-                    error!("Reader Disconnected");
-                    return None;
-                }
-
-                Err(TryRecvError::Empty) => {
-                    // this is expected. we process frames faster than they arrive
-                    return Some(Duration::from_millis(50));
-                }
-            }
-        }
-    }
-
-    fn handle_reader_error(&mut self, e: io::Error) -> Option<Duration> {
-        use io::ErrorKind::*;
-        match e.kind() {
-            InvalidData => {
-                self.err = Some(format!("{e}"));
-                error!("{e:?}");
-                // this usually happens when the reading thread just started up,
-                // and got the very first (clipped) line. We just try again!
-                Some(Duration::from_millis(50))
-            }
-            TimedOut => {
-                self.err = Some(format!("{e}"));
-                error!("{e:?}");
-                // no need to redraw, but keep listening
-                Some(Duration::from_secs(1))
-            }
-            _ => {
-                self.err = Some(format!("Reader encounterd an error: {e}"));
-                error!("{e:?}");
-                let (_, reader_tx) = self.reader_comm.as_ref().unwrap_or_else(|| unreachable!());
-                let _ = reader_tx.send(Commands::STOP);
-                None
-            }
         }
     }
 
@@ -179,22 +58,20 @@ impl MyApp {
             ui.end_row();
 
             ui.label("");
-            if let Some((_, ref sender)) = self.reader_comm {
+            if self.reader.running() {
                 if egui::Button::new("Stop reading")
                     .min_size(size)
                     .ui(ui)
                     .clicked()
                 {
-                    let _ = sender.send(Commands::STOP);
+                    self.reader.stop_reading()
                 }
             } else if egui::Button::new("Start reading")
                 .min_size(size)
                 .ui(ui)
                 .clicked()
             {
-                if let Err(e) = self.spawn_reader() {
-                    self.err = Some(format!("{e}"));
-                }
+                self.reader.start_reading(&self.path, self.baud)
             }
             ui.end_row();
 
@@ -204,7 +81,7 @@ impl MyApp {
                 .ui(ui)
                 .clicked()
             {
-                self.data.clear()
+                self.reader.data.clear()
             }
             ui.end_row();
 
@@ -215,7 +92,7 @@ impl MyApp {
         });
 
         ui.label("Boards:");
-        for board_id in self.data.keys().map(|(b, _)| b).sorted().dedup() {
+        for board_id in self.reader.data.keys().map(|(b, _)| b).sorted().dedup() {
             let mut selected = self.boards.contains(board_id);
             ui.toggle_value(&mut selected, format!("Show Board {board_id}"));
             if selected {
@@ -225,7 +102,7 @@ impl MyApp {
             }
         }
         ui.label("Sensors:");
-        for sensor_id in self.data.keys().map(|(_, s)| s).sorted().dedup() {
+        for sensor_id in self.reader.data.keys().map(|(_, s)| s).sorted().dedup() {
             let mut selected = self.sensors.contains(sensor_id);
             ui.toggle_value(&mut selected, format!("Show Sensor {sensor_id}"));
             if selected {
@@ -240,6 +117,7 @@ impl MyApp {
         let plot = Plot::new("sensor_plt").legend(Legend::default());
         plot.show(ui, |plt_ui| {
             for ((board_id, sensor_id), data) in self
+                .reader
                 .data
                 .iter()
                 .filter(|((b, s), _)| self.boards.contains(b) && self.sensors.contains(s))
@@ -252,14 +130,20 @@ impl MyApp {
         });
     }
 
-    fn save_data(&self, path:&Path) -> Result<(), io::Error> {
+    fn save_data(&self, path: &Path) -> Result<(), io::Error> {
         info!("saving to {path:?}");
         let mut wtr = csv::Writer::from_path(path)?;
-        wtr.write_record(&["Sensor id", "Board id", "Time", "Value"])?;
-        for slice in self.data.keys().flat_map(|(b, s)| {
-            let values = self.data.get(&(*b, *s)).unwrap_or_else(||unreachable!());
-            values.iter().map(|[t,v]| [s.to_string(), b.to_string(), t.to_string(), v.to_string()])
-        }){
+        wtr.write_record(["Sensor id", "Board id", "Time", "Value"])?;
+        for slice in self.reader.data.keys().flat_map(|(b, s)| {
+            let values = self
+                .reader
+                .data
+                .get(&(*b, *s))
+                .unwrap_or_else(|| unreachable!());
+            values
+                .iter()
+                .map(|[t, v]| [s.to_string(), b.to_string(), t.to_string(), v.to_string()])
+        }) {
             wtr.write_record(&slice)?;
         }
         wtr.flush()
@@ -268,19 +152,19 @@ impl MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        if self.recive_data().is_some() {
-            ctx.request_repaint_after(Duration::from_millis(100));
+        if let Some(d) = self.reader.process() {
+            ctx.request_repaint_after(d);
         }
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| self.show_sidebar(ui));
         egui::CentralPanel::default().show(ctx, |ui| self.show_plot(ui));
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-            self.err.as_ref().map(|err| ui.label(err));
+            ui.label(self.reader.reader_status());
         });
 
         self.file_dialog.show(ctx);
-        if self.file_dialog.selected(){
-            if let Some(path) =  self.file_dialog.path(){
+        if self.file_dialog.selected() {
+            if let Some(path) = self.file_dialog.path() {
                 self.save_data(path).unwrap();
             }
         }
